803be04995d632a2efce00652ed8adfcab2b5fad
hyeseungS <hyeseung2000@gmail.com>
2022-03-28 19:13:22 +0900

    Initial commit

---
+++ src/main/java/com/hotpotato/blueroof/BlueroofApplication.java
@@ -0,0 +1,13 @@
+package com.hotpotato.blueroof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueroofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueroofApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotpotato/blueroof/BlueroofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotpotato.blueroof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueroofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
